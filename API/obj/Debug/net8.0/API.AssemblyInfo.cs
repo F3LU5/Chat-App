83e8b571a8f6962f4ee1799c96fbf9db40386b63
Tomasz Fela <tomasz.fela10@gmail.com>
2024-12-18 01:01:21 +0100

    uporządkowanie kodu

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac13ed9f255290ebb3e5eebcc562c79b1046a2a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4bd7399605bc9522822fe48bacca2d3260fcc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
