a2b0188ba14c778e5d2cee1966864ade255d8e08
Tomasz Fela <tomasz.fela10@gmail.com>
2024-12-29 23:49:20 +0100

    ladowanie pacman edytowanie profilu

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31291110435f23cb50cde1865e3ce4d4693283b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2249b11fd10fbfc2a229ada5d9181a416b896c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
